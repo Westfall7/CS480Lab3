a353ba1cdbba5fa411e3d37909f639551fdbecdb
Westfall7 <44597865+Westfall7@users.noreply.github.com>
2018-10-30 18:59:23 -0700

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -5,10 +5,15 @@ import javax.script.ScriptException;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import javax.swing.border.EmptyBorder;
 
 //class catButton
 public class Calculator extends JFrame
 {
+   //size of window
+   private final int WINDOW_WIDTH = 300;
+   private final int WINDOW_HEIGHT = 400;
+   
    //declare the different buttons
    private JButton button1;
    private JButton button2;
@@ -31,83 +36,66 @@ public class Calculator extends JFrame
    private JButton buttonc;
    private JButton buttonPM;
    private JButton buttonDot;
+   private JTextField txtDis;
+   
+   private JPanel motherPanel;
+   private JPanel textPanel;
+   private JPanel buttons;
 
-   
-   private ImageIcon image;
-   private JLabel imageLabel;   
-   
-   //size of window
-   private final int WINDOW_WIDTH = 800;
-   private final int WINDOW_HEIGHT = 700;
+   String equationString;
    
    //Calculator constructor
    public Calculator()
    {
-      //set title, size, close operation, type of layout
-      //build the panel and then set visible
+      createButtons();
+      editFont();
+           
       setTitle("Calculator");
       setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+      setLocationByPlatform(true);
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setLayout(new GridLayout(5, 4));
-      buildPanel();
       setVisible(true);
-
-   }
-   
-   //the method that instantites the components
-   private void buildPanel()
-   {
-      button1 = new JButton("1");
-      button2 = new JButton("2");
-      button3 = new JButton("3");
-      button4 = new JButton("4");
-      button5 = new JButton("5");
-      button6 = new JButton("6");
-      button7 = new JButton("7");
-      button8 = new JButton("8");
-      button9 = new JButton("9");
-      button0 = new JButton("0");
-      buttonP = new JButton("+");
-      buttonM = new JButton("-");
-      buttonMult = new JButton("*");
-      buttonDiv = new JButton("/");
-      buttonSqur = new JButton("^");
-      buttonPar = new JButton("()");
-      buttonEqu = new JButton("=");
-      buttonback = new JButton("<----");
-      buttonc = new JButton("C");
-      buttonPM = new JButton("+-");
-      buttonDot = new JButton(".");
       
+      addItems();
       
-      //set newfact button and its actionlistener
-      //newFact = new JButton("Click for new facts");
-      //newFact.addActionListener(new FactSwitch());      
-
-      //add all the buttons to the grid layout       
-      add(buttonSqur);
-      add(buttonc);
-      add(buttonback);
-      add(buttonDiv);
-      add(button7);
-      add(button8);
-      add(button9);
-      add(buttonMult);
-      add(button4);
-      add(button5);
-      add(button6);
-      add(buttonM);
-      add(button1);
-      add(button2);
-      add(button3);
-      add(buttonP);      
-      add(buttonPar);
-      add(button0);
-      add(buttonDot);
-      add(buttonEqu);
-
+      button1.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent e){
+               equationString = (equationString+"1");
+         }
+      });
+      
+            button2.addActionListener(new ActionListener(){
+         public void actionPerformed(ActionEvent e){
+               equationString = (equationString+"2");
+         }
+      });
+      
+      /*
+      button2
+      button3
+      button4
+      button5
+      button6
+      button7
+      button8
+      button9
+      button0
+      buttonP
+      buttonM
+      buttonMult
+      buttonDiv
+      buttonSqur
+      buttonPar
+      buttonEqu
+      buttonback
+      buttonc
+      buttonPM
+      buttonDot
+      */       
    
    }
+   
+   
    
    
 /*   
@@ -165,4 +153,100 @@ public class Calculator extends JFrame
          System.out.println("There was an error in your formula.");
       }
    }
+   
+   public void createButtons(){
+           
+      motherPanel = new JPanel();
+      motherPanel.setLayout(new BoxLayout(motherPanel, BoxLayout.Y_AXIS));
+      textPanel = new JPanel();
+      buttons = new JPanel(new GridLayout(5, 4));
+     
+      //the method that instantites the components
+      txtDis = new JTextField();
+      
+      button1 = new JButton("1");
+      button2 = new JButton("2");
+      button3 = new JButton("3");
+      button4 = new JButton("4");
+      button5 = new JButton("5");
+      button6 = new JButton("6");
+      button7 = new JButton("7");
+      button8 = new JButton("8");
+      button9 = new JButton("9");
+      button0 = new JButton("0");
+      buttonP = new JButton("+");
+      buttonM = new JButton("-");
+      buttonMult = new JButton("*");
+      buttonDiv = new JButton("/");
+      buttonSqur = new JButton("^");
+      buttonPar = new JButton("()");
+      buttonEqu = new JButton("=");
+      buttonback = new JButton("<----");
+      buttonc = new JButton("C");
+      buttonPM = new JButton("+-");
+      buttonDot = new JButton(".");
+   }
+   
+   public void editFont(){
+   //set font for all the buttons and text box
+      txtDis.setFont(new Font("Calibri", Font.BOLD, 20));
+      txtDis.setPreferredSize(new Dimension(270, 50));
+      txtDis.setBackground(Color.WHITE);
+      txtDis.setEnabled(false);
+      txtDis.setHorizontalAlignment(4); 
+   
+      button1.setFont(new Font("Calibri", Font.BOLD, 20));
+      button2.setFont(new Font("Calibri", Font.BOLD, 20));
+      button3.setFont(new Font("Calibri", Font.BOLD, 20));
+      button4.setFont(new Font("Calibri", Font.BOLD, 20));
+      button5.setFont(new Font("Calibri", Font.BOLD, 20));
+      button6.setFont(new Font("Calibri", Font.BOLD, 20));
+      button7.setFont(new Font("Calibri", Font.BOLD, 20));
+      button8.setFont(new Font("Calibri", Font.BOLD, 20));
+      button9.setFont(new Font("Calibri", Font.BOLD, 20));
+      button0.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonP.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonM.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonMult.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonDiv.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonSqur.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonPar.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonEqu.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonback.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonc.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonPM.setFont(new Font("Calibri", Font.BOLD, 20));
+      buttonDot.setFont(new Font("Calibri", Font.BOLD, 20));
+   }
+   
+   public void addItems(){
+      //add text box to textPanel
+      textPanel.add(txtDis);
+      //add all the buttons to the grid layout        
+      buttons.add(buttonSqur);
+      buttons.add(buttonc);
+      buttons.add(buttonback);
+      buttons.add(buttonDiv);
+      buttons.add(button7);
+      buttons.add(button8);
+      buttons.add(button9);
+      buttons.add(buttonMult);
+      buttons.add(button4);
+      buttons.add(button5);
+      buttons.add(button6);
+      buttons.add(buttonM);
+      buttons.add(button1);
+      buttons.add(button2);
+      buttons.add(button3);
+      buttons.add(buttonP);      
+      buttons.add(buttonPar);
+      buttons.add(button0);
+      buttons.add(buttonDot);
+      buttons.add(buttonEqu);
+      
+      
+      motherPanel.add(textPanel);
+      motherPanel.add(buttons);
+      add(motherPanel);
+   }
+   
 }
